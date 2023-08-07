a632164e9d6ed09f2136347d300015c60ac05539
eliabn <30064396@tafe.wa.edu.au>
2023-08-07 16:29:08 +0930

    Added getForecast and displayForecast functions

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,7 @@ function getName() {
     } else {
         console.log("Got city");
         getWeather(city);
+        getForecast(city)
     }
     console.log(city);
 }
@@ -82,3 +83,70 @@ function displayWeather(data) {
     <p id="H">Humidity: ${main.humidity}%</p>
     <button class="btn btn-primary">More info.</button>`
 }
+
+
+
+
+
+// Fetch the forecast
+function getForecast(city) {
+
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&lang=en&units=metric&appid=' + apiKey
+
+    fetch(url)
+      .then((resp) => {
+        //console.log(resp);
+        if (!resp.ok) {
+          throw new Error("Weather data could not be fetched. Status: " + resp.statusText);
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        // Handle the fetched weather data here
+        displayForecast(data)
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        // Handle the error here or display an error message to the user
+});
+}
+
+function displayForecast(data) {
+    console.log(data)
+
+    // Weather information
+    let main = data.list[0].dt_txt
+
+    //let time = data.list[0].dt_txt.split(" ")[1]
+
+    //console.log(time)
+
+    let card = document.querySelector('.forecast-cards')
+
+    let index = -1
+    card.innerHTML = data.list.map(day => {
+        index++
+        let time = data.list[index].dt_txt.split(" ")[1];
+        //let time = dateTime[1];
+        if (time === '12:00:00') {
+            // Weather property of each day
+            let day = data.list[index]
+
+            // Store date of day "ddd, mmm, DD"
+            let date = new Date(day.dt *1000).toDateString().split(' 20')[0];
+            console.log(day)
+
+            //let date = dateTime[0].split('-')[2]
+            return `<div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
+            <div class="card p-2 ">
+              <h3 class="card-header">${date}</h3>
+              <p id="T">Temp: ${day.main.temp}°C</p>
+              <p id="W">Wind: ${day.wind.speed} mph</p>
+              <p id="H">Humidity: ${day.main.humidity}%</p>
+              <button class="btn btn-block btn-primary">Learn more.</button>
+            </div>
+        </div>`
+        }
+    }).join(' ');
+}
+
