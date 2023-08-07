d64feaa5ce2efc5b305961357c49948c7ce5a949
eliabn <30064396@tafe.wa.edu.au>
2023-08-07 13:14:17 +0930

    Added getWeather and displayWeather function

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,109 @@ function getName() {
         console.log("Enter City");
     } else {
         console.log("Got city");
-        getWeather(city);
+        //getWeather(city);
+        getForecast(city)
     }
     console.log(city);
 }
+
+
+// Fetch the weather
+function getWeather(city) {
+
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&lang=en&units=metric&appid=' + apiKey
+
+    fetch(url)
+      .then((resp) => {
+        console.log(resp);
+        if (!resp.ok) {
+          throw new Error("Weather data could not be fetched. Status: " + resp.statusText);
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // Handle the fetched weather data here
+        displayWeather(data)
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        // Handle the error here or display an error message to the user
+});
+}
+
+
+
+function displayWeather(data) {
+    console.log(data)
+    let date = new Date(data.dt *1000);
+    console.log(date)
+
+    // Weather information
+    let main = data.main
+
+    let display = document.querySelector('.city-info')
+
+    display.innerHTML = `<h1 class="city-date p-0">${data.name} ${date.toDateString()}</h1>
+    <p id="T">Temp: ${main.temp}°C</p>
+    <p id="W">Wind: ${data.wind.speed} mph</p>
+    <p id="H">Humidity: ${main.humidity}%</p>
+    <button class="btn btn-primary">More info.</button>`
+}
+
+
+
+
+
+
+
+
+
+// Fetch the forecast
+function getForecast(city) {
+
+    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&lang=en&units=metric&appid=' + apiKey
+
+    fetch(url)
+      .then((resp) => {
+        //console.log(resp);
+        if (!resp.ok) {
+          throw new Error("Weather data could not be fetched. Status: " + resp.statusText);
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        // Handle the fetched weather data here
+        displayForecast(data)
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        // Handle the error here or display an error message to the user
+});
+}
+
+function displayForecast(data) {
+    console.log(data)
+
+    // Weather information
+    let main = data.list[0].dt_txt
+
+    //let time = data.list[0].dt_txt.split(" ")[1]
+
+    //console.log(time)
+
+    let card = document.querySelector('.forecast-cards')
+
+    let temp = -1
+    card.innerHTML = data.list.map(day => {
+        temp++
+        let time = data.list[temp].dt_txt.split(" ")[1]
+        if (time === '12:00:00') {
+            return `<p id="T">Temp: ${temp}*</p>`
+        } 
+
+        
+        
+    }).join(' ');
+}
+
